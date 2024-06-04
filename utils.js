368a0d8a82f1fae4f2b230fb1ab5087af76ddc8c
BradleyHu0707 <bradleyhu0707@gmail.com>
2024-06-04 08:34:42 -0600

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,18 @@
+class UTILS {
+    constructor() {
+    }
+
+    getInfo() {
+        return {
+            "id": "UTILS",
+            "name": "UTILS",
+            "blocks": [{ "opcode": "commentBlock", "blocktype": "block", "text": "Comment:[txt1]", "arguments": {"txt1": {"type": "string", "defaultValue": "Comment"}}},{"opcode": "inlinecom", "blockType": "reporter", "text": "comment:[txt2] value:[actv1]", "arguments": {"txt2": {"type": "string", "defaultValue": "Inline Comment"}, "actv1": {"type": "string", "defaultValue": "Actual Value"}}},{"opcode": "unused", "blockType": "hat", "text": "Unused Code/Thread Tester", "arguments": null},{"opcode": "nullReport", "blockType": "reporter", "text": "null", "arguments": null},{"opcode": "bruh", "blockType": "bruh", "text": "bruh", "arguments":null}]
+        }
+    }
+    inlinecom({txt2,actv1}){return actv1}
+    nullReport(){return null}
+    trueReport(){return true}
+    falseReport(){return false}
+}
+
+Scratch.extensions.register(new UTILS())
